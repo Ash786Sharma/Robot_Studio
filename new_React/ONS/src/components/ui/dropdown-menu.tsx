5e9d72fbb4ecbfe6b7f070238532d87f3e832baf
Ashwani Sharma <35649342+Ash786Sharma@users.noreply.github.com>
2026-09-07 11:55:27 +0100

    Update dropdown-menu.tsx

--- new_React/ONS/src/components/ui/dropdown-menu.tsx
+++ new_React/ONS/src/components/ui/dropdown-menu.tsx
@@ -128,17 +128,42 @@ function DropdownMenuSubContent({
   sideOffset = 0,
   className,
   ...props
-}: React.ComponentProps<typeof DropdownMenuContent>) {
+}: MenuPrimitive.Popup.Props &
+  Pick<
+    MenuPrimitive.Positioner.Props,
+    "align" | "alignOffset" | "side" | "sideOffset"
+  >) {
   return (
-    <DropdownMenuContent
+    <MenuPrimitive.Positioner
       data-slot="dropdown-menu-sub-content"
-      className={cn("w-auto min-w-[96px] rounded-lg bg-popover p-1 text-popover-foreground shadow-lg ring-1 ring-foreground/10 duration-100 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95", className )}
+      className="isolate z-50 outline-none"
       align={align}
       alignOffset={alignOffset}
       side={side}
       sideOffset={sideOffset}
-      {...props}
-    />
+    >
+      <MenuPrimitive.Popup
+        className={cn(
+          "z-50 min-w-32 origin-(--transform-origin)",
+          "overflow-x-hidden overflow-y-auto",
+          "rounded-lg bg-popover p-1 text-popover-foreground",
+          "shadow-lg ring-1 ring-foreground/10",
+          "outline-none",
+          "data-[side=bottom]:slide-in-from-top-2",
+          "data-[side=left]:slide-in-from-right-2",
+          "data-[side=right]:slide-in-from-left-2",
+          "data-[side=top]:slide-in-from-bottom-2",
+          "data-open:animate-in",
+          "data-open:fade-in-0",
+          "data-open:zoom-in-95",
+          "data-closed:animate-out",
+          "data-closed:fade-out-0",
+          "data-closed:zoom-out-95",
+          className
+        )}
+        {...props}
+      />
+    </MenuPrimitive.Positioner>
   )
 }
 
